cbc4e1f51c20cacd51d30271678b4524268467c8
masanari9256-company <masanari92562@gmail.com>
2022-03-11 17:42:35 +0900

    [update]タグモデルの作成と記事モデルの編集

--- laravel/app/Article.php
+++ laravel/app/Article.php
@@ -39,4 +39,8 @@ class Article extends Model
     public function getCountLikesAttribute(): int {
         return $this->likes->count();
     }
+
+    public function tags(): BelongsToMany {
+        return $this->belongsToMany('App/Tag')->withTimestamps();
+    }
 }
